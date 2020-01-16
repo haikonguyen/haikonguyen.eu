ba34015b309770079c2707ef96d0e4977ea7bd52
Hai Ha Nguyen <haicorp87@gmai.com>
2020-01-16 21:03:21 +0100

    added StyledComponents support, basic SiteConfig settings

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,19 +1,41 @@
-import React from "react";
-import Helmet from "react-helmet";
-import config from "../../data/SiteConfig";
-import "./index.css";
+import React from 'react';
+import Helmet from 'react-helmet';
+import styled, { createGlobalStyle } from 'styled-components';
+import config from '../../data/SiteConfig';
+import Navigation from '../components/Navigation/Navigation.component';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const StyledLayout = styled.div`
+  color: red;
+`;
 
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div className="layout-container">
+      <StyledLayout>
+        <GlobalStyle />
         <Helmet>
-          <meta name="description" content={config.siteDescription} />
-          <html lang="en" />
+          <meta name='description' content={config.siteDescription} />
+          <html lang='en' />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+          />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          />
         </Helmet>
+        <Navigation />
         {children}
-      </div>
+      </StyledLayout>
     );
   }
 }
