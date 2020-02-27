0336a0457465bbcc31f3454cf87fedd145adc40e
Hai Ha Nguyen <haicorp87@gmai.com>
2020-02-27 01:12:51 +0100

    adding the react-typed

--- src/components/Hero/Hero.component.jsx
+++ src/components/Hero/Hero.component.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import Typed from 'react-typed';
+import Typography from '@material-ui/core/Typography';
 import bgSrc from '../../images/heroBg.jpg';
+
+const textLines = [`Web Developer`, `Fotographer`, `VLOGGER`];
 
 const StyledBgWrap = styled.section`
   background-image: url(${bgSrc});
   background-size: cover;
   background-position: center center;
+  /* background-attachment: fixed; */
   width: 100%;
   height: 100vh;
   display: flex;
   align-items: flex-end;
 
   .hero-content {
-    bottom: 0;
     background: rgba(0, 0, 0, 0.5);
-    color: #f1f1f1;
+    color: ${props => props.theme.colors.white};
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
@@ -22,6 +26,15 @@ const StyledBgWrap = styled.section`
     flex-direction: column;
     padding: 20px;
     width: 100%;
+
+    @media ${props => props.theme.screen.tablet} {
+      display: block;
+      width: 350px;
+      position: absolute;
+      top: 50%;
+      left: 10%;
+      text-align: center;
+    }
   }
 `;
 
@@ -29,11 +42,14 @@ const Hero = () => {
   return (
     <StyledBgWrap>
       <div className='hero-content'>
-        <h1>Heading</h1>
-        <p>
-          Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat
-          phaedrum te duo, eum cu recteque expetendis neglegentur.
-        </p>
+        <Typography component='div' align='center'>
+          <div>Hi I am</div>
+          <div>Haiko Nguyen</div>
+          <div>
+            I am a <Typed strings={textLines} typeSpeed={100} />
+          </div>
+          <div>Contact Me</div>
+        </Typography>
       </div>
     </StyledBgWrap>
   );
