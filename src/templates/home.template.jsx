d7e1b4137a87a6423e3e0fe919f4fdf19b41a702
Hai Ha Nguyen <haicorp87@gmai.com>
2020-03-09 16:50:10 +0100

    adding basic structure of about section

--- src/templates/home.template.jsx
+++ src/templates/home.template.jsx
@@ -2,27 +2,75 @@ import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import styled from 'styled-components';
 import { rem } from 'polished';
+import Button from '@material-ui/core/Button';
+import profileimg from '../images/HaikoProfile.jpg';
 import Hero from '../components/Hero/Hero.component';
 import MainLayout from '../layout/index';
 import PostListing from '../components/PostListing/PostListing';
 
-const StyledContainer = styled(Container)`
-  h1 {
-    color: ${props => props.theme.colors.white};
-    text-align: center;
-    margin: 0;
-    text-transform: uppercase;
-  }
+const StyledFixedContainer = styled(Container)`
+  padding: ${rem('70px')} 0 !important;
 
-  hr {
-    border: ${rem('1px')} solid ${props => props.theme.colors.primaryThemeColor};
-    width: ${rem('100px')};
-    margin: 0 auto;
-    margin-top: ${rem('10px')};
-  }
+  .heading {
+    padding-bottom: ${rem('70px')};
 
-  .section {
-    padding: ${rem('70px')} 0;
+    h1 {
+      color: ${props => props.theme.colors.white};
+      text-align: center;
+      margin: 0;
+      text-transform: uppercase;
+    }
+
+    hr {
+      border: ${rem('1px')} solid
+        ${props => props.theme.colors.primaryThemeColor};
+      width: ${rem('100px')};
+      margin: 0 auto;
+      margin-top: ${rem('10px')};
+    }
+  }
+`;
+
+const StyledFluidContainer = styled(Container)`
+  .col {
+    margin-bottom: ${rem('30px')};
+  }
+  .about {
+    h2 {
+      font-size: ${rem('25px')};
+    }
+    .profilePicture {
+      width: 100%;
+      position: relative;
+      z-index: 10;
+      border-radius: ${rem('10px')};
+
+      &--designElement {
+        display: none;
+        border: 1px solid white;
+        height: ${rem('350px')};
+        width: ${rem('350px')};
+        position: absolute;
+        top: ${rem('-22px')};
+        left: ${rem('-10px')};
+        border-radius: ${rem('10px')};
+
+        @media ${props => props.theme.screen.desktop} {
+          display: block;
+        }
+      }
+    }
+
+    .button {
+      color: #90caf9;
+      border: ${rem('1px')} solid
+        ${props => props.theme.colors.primaryThemeColor};
+
+      &:hover {
+        border: ${rem('1px')} solid #90caf9;
+        background-color: rgba(144, 202, 249, 0.08);
+      }
+    }
   }
 `;
 
@@ -31,22 +79,64 @@ const Home = ({ showOnScroll, data }) => {
     <MainLayout>
       {/* Hero Section */}
       <Hero showOnScroll={showOnScroll} />
-      <StyledContainer>
-        <Row className='section'>
-          {/* About Section */}
-          <Col xs={12}>
-            <h1>About</h1>
-            <hr />
-          </Col>
+
+      {/* About Section */}
+      <StyledFluidContainer fluid>
+        <Row>
+          <StyledFixedContainer className='about'>
+            <Col xs={12}>
+              <div className='heading'>
+                <h1>About</h1>
+                <hr />
+              </div>
+              <Row>
+                <Col xs={12} md={5}>
+                  <div className='profilePictureWrap'>
+                    <img
+                      className='profilePicture'
+                      src={profileimg}
+                      alt='profile avatar'
+                    />
+                    <div className='profilePicture--designElement' />
+                  </div>
+                </Col>
+                <Col xs={12} offset={{ md: 1 }} md={6}>
+                  <h2>HI, I'M HAIKO</h2>
+                  <p>
+                    It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout. The point of using Lorem Ipsum is that it has a
+                    more-or-less normal distribution of letters, as opposed to
+                    using 'Content here, content here', making it look like
+                    readable English. Many desktop publishing packages and web
+                    page editors now use Lorem Ipsum as their default model
+                    text, and a search for 'lorem ipsum' will uncover many web
+                    sites still in their infancy. Various versions have evolved
+                    over the years, sometimes by accident, sometimes on purpose
+                    (injected humour and the like).
+                  </p>
+                  <Button className='button' size='large' variant='outlined'>
+                    Read My Story
+                  </Button>
+                </Col>
+              </Row>
+            </Col>
+          </StyledFixedContainer>
         </Row>
-        <Row className='section'>
+
+        <Row>
           {/* Blog Section */}
-          <Col xs={12}>
-            <h1>Blog</h1>
-            <PostListing data={data} />
-          </Col>
+          <StyledFixedContainer className='blog'>
+            <Col xs={12}>
+              <div className='heading'>
+                <h1>Blog</h1>
+                <hr />
+              </div>
+              <PostListing data={data} />
+            </Col>
+          </StyledFixedContainer>
         </Row>
-      </StyledContainer>
+      </StyledFluidContainer>
     </MainLayout>
   );
 };
