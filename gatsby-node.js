7c491a36d128fed30314927fa9cc8e089b552b24
Hai Ha Nguyen <haicorp87@gmai.com>
2020-03-12 01:37:01 +0100

    adding basic wrapper to post template, removing unused code, updating nav styles

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,7 +41,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const postPage = path.resolve('src/templates/post.jsx');
+  const postPage = path.resolve('src/templates/post.template.jsx');
   const tagPage = path.resolve('src/templates/tag.jsx');
   const categoryPage = path.resolve('src/templates/category.jsx');
   const homePage = path.resolve('src/templates/home.template.jsx');
