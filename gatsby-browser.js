dc1393b1e9a3089508443a52195a88936af3a84f
Hai Ha Nguyen <haicorp87@gmai.com>
2020-03-26 22:42:51 +0100

    ignore jsx warning

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@ import UiContextProvider from './src/context/ui.context';
 
 export const wrapRootElement = ({ element }) => {
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <ThemeContextProvider>
       <UiContextProvider>{element}</UiContextProvider>
     </ThemeContextProvider>
