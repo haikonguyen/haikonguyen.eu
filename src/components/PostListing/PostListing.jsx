28344b547e49a0e2914496681fbcbbd028f5c7c8
Hai Ha Nguyen <haicorp87@gmai.com>
2020-04-04 18:58:03 +0200

    FIXING the NetlifyCMS nightmare aka fixed path for content and images ---> static folder

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -1,57 +1,9 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import styled from 'styled-components';
-import { rem } from 'polished';
-
-const StyledContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  max-width: 1140px;
-  margin: 0 auto;
-  grid-gap: 20px;
-
-  @media ${props => props.theme.screen.tablet} {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  @media ${props => props.theme.screen.laptopL} {
-    grid-template-columns: repeat(3, 1fr);
-  }
-`;
-
-const StyledCard = styled(Card)`
-  && {
-    background-color: ${props => props.theme.colors.cardItemBg};
-    color: ${props => props.theme.colors.bodyTextColor};
-    box-shadow: ${props => props.theme.colors.cardItemShadow};
-  }
-
-  .media {
-    height: ${rem('140px')};
-  }
-
-  .cardContent {
-    min-height: ${rem('172px')};
-
-    h2 {
-      font-size: ${rem('24px')};
-    }
-
-    p {
-      font-size: ${rem('16px')};
-      line-height: ${rem('22px')};
-      text-align: justify;
-    }
-  }
-  .styledLink {
-    color: ${props => props.theme.colors.primaryThemeColor};
-    text-decoration: none !important;
-  }
-`;
+import { StyledContainer, StyledCard } from './PostListing.style';
 
 const PostListing = () => {
   const data = useStaticQuery(graphql`
@@ -84,7 +36,7 @@ const PostListing = () => {
   const getPostList = () => {
     const postEdges = data.allMarkdownRemark.edges;
     const postList = [];
-    postEdges.forEach(postEdge => {
+    postEdges.forEach((postEdge) => {
       postList.push({
         path: postEdge.node.fields.slug,
         tags: postEdge.node.frontmatter.tags,
@@ -93,7 +45,7 @@ const PostListing = () => {
         date: postEdge.node.fields.date,
         excerpt: postEdge.node.excerpt,
         timeToRead: postEdge.node.timeToRead,
-        id: postEdge.node.id
+        id: postEdge.node.id,
       });
     });
     return postList;
@@ -101,27 +53,31 @@ const PostListing = () => {
 
   const postList = getPostList();
 
+  console.log(postList);
+
   return (
     <StyledContainer>
-      {postList.map(post => (
-        <Link key={post.id} className='styledLink' to={post.path}>
-          <StyledCard>
-            <CardActionArea>
-              <CardMedia
-                className='media'
-                image={post.cover}
-                title={post.title}
-              >
-                <img src={post.cover} alt={`${post.title} cover`} />
-              </CardMedia>
-              <CardContent className='cardContent'>
-                <h2>{post.title}</h2>
-                <p>{post.excerpt}</p>
-              </CardContent>
-            </CardActionArea>
-          </StyledCard>
-        </Link>
-      ))}
+      {postList.map((post) => {
+        return (
+          <Link key={post.id} className='styledLink' to={post.path}>
+            <StyledCard>
+              <CardActionArea>
+                <CardMedia
+                  className='media'
+                  image={post.cover}
+                  title={post.title}
+                >
+                  <img src='' alt={`${post.title} cover`} />
+                </CardMedia>
+                <CardContent className='cardContent'>
+                  <h2>{post.title}</h2>
+                  <p>{post.excerpt}</p>
+                </CardContent>
+              </CardActionArea>
+            </StyledCard>
+          </Link>
+        );
+      })}
     </StyledContainer>
   );
 };
